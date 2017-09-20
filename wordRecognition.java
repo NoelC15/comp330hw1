70442d693dd5559b3a99b630a3b6c33b7c8601e1
Noel <castillonoel15@yahoo.com>
2017-09-20 22:19:43 +0000

    Cleaned and organized code for DataFile and wordRecognition

--- wordRecognition.java
+++ wordRecognition.java
@@ -1,40 +1,101 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class wordRecognition {
 	
+	public ArrayList<String> List;
 	public String File;
+	
+	wordRecognition(){
+		File = "File can not be read";
+		List = new ArrayList<String>();
+	}
 	
 	wordRecognition(String newFile){
 		File = newFile;
 	}
 	
 	public void setFile(String file){
-		this.File = file;
+		File = file;
 	}
 	
 	public String getFile(){
 		return File;
 	}
+	
+	public void setArrayList(ArrayList<String> list){
+		List = list;
+	}
+	
+	public ArrayList<String> getList(){
+		return List;
+	}
 		
 	public void CountWord(){
-		String str = File;
+		String [] stringArr = List.toArray(new String[0]);
+		String str = Arrays.toString(stringArr);
         String[] splitStr = str.split(" *(,|=>| |\\.) *" );
         int count =0;
-        List<String> list = new ArrayList<>();
+        List<String> List2 = new ArrayList<>();
         for(String s:splitStr){
-            if(!list.contains(s)){
-                list.add(s);
+            if(!List2.contains(s)){
+                List2.add(s);
             }
         }
-        for(int i=0;i<list.size();i++){
+        for(int i=0;i<List2.size();i++){
             for(int j=0;j<splitStr.length;j++){
-                if(list.get(i).equals(splitStr[j])){
+                if(List2.get(i).equals(splitStr[j])){
                     count++;
                 }
             }
-            System.out.println("Occurrence of " + list.get(i) + " is " + count + " times.");
+            System.out.println("Occurrence of <" + List2.get(i) + "> is " + count + " times.");
             count = 0;
         }	
-	}	
+	}
+	
+	public void ReadInFile(){
+		BufferedReader br = null;
+		FileReader fr = null;
+
+		try {
+
+			fr = new FileReader(File);
+			br = new BufferedReader(fr);
+			
+			String sCurrentLine;
+			while ((sCurrentLine = br.readLine()) != null) {
+				System.out.println(sCurrentLine);
+				addToList(sCurrentLine);
+			}
+		
+		} catch (IOException e) {
+
+			e.printStackTrace();
+
+		} finally {
+
+			try {
+
+				if (br != null)
+					br.close();
+
+				if (fr != null)
+					fr.close();
+
+			} catch (IOException ex) {
+
+				ex.printStackTrace();
+
+			}
+
+		}
+	}
+	
+	public void addToList(String ListToAdd){
+		List.add(ListToAdd);
+	}
 }
